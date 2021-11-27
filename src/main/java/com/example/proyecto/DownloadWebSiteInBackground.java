b6c626d41e12f155c00c306f5ee238e5e95377f2
LlucSerramitjana <80621909+LlucSerramitjana@users.noreply.github.com>
2021-11-27 17:32:49 +0100

    Add files via upload

---
+++ src/main/java/com/example/proyecto/DownloadWebSiteInBackground.java
@@ -0,0 +1,13 @@
+package com.example.proyecto;
+import android.os.AsyncTask;
+public class DownloadWebSiteInBackground extends AsyncTask<String, Void, String>{
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+}
---
+++ src/main/java/com/example/proyecto/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button bLogin;
+    EditText etUsername, etPassword;
+    TextView tvRegisterLink;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        bLogin = (Button) findViewById(R.id.bLogin);
+        tvRegisterLink = (TextView) findViewById(R.id.tvRegisterLink);
+
+        bLogin.setOnClickListener(this);
+        tvRegisterLink.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bLogin:
+
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+
+                break;
+        }
+    }
+
+
+}
